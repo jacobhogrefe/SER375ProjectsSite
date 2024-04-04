07784bfb80d3b31e2db4662457ec81e377d7c662
a-r-t <alexthimineur@gmail.com>
2024-04-04 17:51:01 -0400

    clean up project.vue file

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,37 +1,36 @@
 <template>
-    <div>
-      <div v-if="isValidProject">
-        <component :is="projectTitle"></component>
-      </div>
-      <div v-else>
-        <h1 style="padding: 45vh;">Invalid project!</h1>
-      </div>
+  <div>
+    <div v-if="isValidProject">
+      <component :is="projectTitle"></component>
     </div>
-  </template>
+    <div v-else>
+      <h1 style="padding: 45vh;">Invalid project!</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+const { getProjectComponents } = require('../projects/projects-gatherer.js')
+const projectComponents = getProjectComponents()
   
-  <script>
-  const { getProjectComponents } = require('../projects/projects-gatherer.js')
-  const projectComponents = getProjectComponents()
-    
-  export default {
-    name: 'Project',
-    components: {
-      ...projectComponents
-    },
-    computed: {
-      isValidProject() {
-        return !!Object.keys(projectComponents).find(projectComponentKey => projectComponentKey === this.projectTitle)
-      }
-    },
-    mounted() {
-      console.log(this.$route.params.projectTitle)
-      this.projectTitle = this.$route.params.projectTitle
-    },
-    data() {
-      return {
-        projectTitle: ''
-      }
+export default {
+  name: 'Project',
+  components: {
+    ...projectComponents
+  },
+  computed: {
+    isValidProject() {
+      return !!Object.keys(projectComponents).find(projectComponentKey => projectComponentKey === this.projectTitle)
+    }
+  },
+  mounted() {
+    console.log(this.$route.params.projectTitle)
+    this.projectTitle = this.$route.params.projectTitle
+  },
+  data() {
+    return {
+      projectTitle: ''
     }
   }
-  </script>
-  ../projects/projects-compiler.js../projects/projects-gatherer.js
+}
+</script>
